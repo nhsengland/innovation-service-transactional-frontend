d8f2899990481358736b1f61969cf55801a6ac60
Ricardo Tavares <ricardo.tavares@bjss.com>
2021-04-06 09:15:21 +0100

    Implemented first time signin decision flow

--- src/app/config/environment.config.ts
+++ src/app/config/environment.config.ts
@@ -9,8 +9,8 @@ const logLevel = 'TRACE';
 
 
 export const environment = {
-  // API_URL: 'https://51.105.20.41', // env.API_URL,
-  API_URL: 'http://localhost:4200', // env.API_URL,
+  API_URL: 'https://51.105.20.41', // env.API_URL,
+  // API_URL: 'http://localhost:4200', // env.API_URL,
   LOG_LEVEL: NgxLoggerLevel[logLevel], // NgxLoggerLevel[(env.LOG_LEVEL as keyof typeof NgxLoggerLevel) || 'ERROR']
   BASE_URL: '/transactional',
 };
