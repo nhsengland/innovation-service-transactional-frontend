99654f4945878551dc3003d1860f81cfe1d5a424
Shalini Nayak <Shalini.Nayak1@nhs.net>
2021-08-18 19:58:41 +0530

    Disable analytics for lower environments

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { enableProdMode } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppInjector } from '@modules/core/injectors/app-injector';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { CookiesService } from '@modules/core';
 
 if (environment.production) {
   enableProdMode();
@@ -10,5 +14,11 @@ if (environment.production) {
 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(ref => {
+    if (!environment.enableAnalytics){
+      const cookiesService = AppInjector.getInjector().get(CookiesService);
+      cookiesService.updateConsentCookie(false);
+    }
+  })
   .catch(err => console.error(err));
 });
