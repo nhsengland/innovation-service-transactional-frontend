7f140928f66820dd818cb01b47a8fa43271e57d4
Shalini Nayak <Shalini.Nayak1@nhs.net>
2021-08-27 18:12:37 +0530

    Toogle feature analytics

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { enableProdMode } from '@angular/core';
-import { Injectable } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppInjector } from '@modules/core/injectors/app-injector';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { CookiesService } from '@modules/core';
 
 if (environment.production) {
   enableProdMode();
@@ -14,11 +11,5 @@ if (environment.production) {
 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule)
-  .then(ref => {
-    if (!environment.enableAnalytics){
-      const cookiesService = AppInjector.getInjector().get(CookiesService);
-      cookiesService.updateConsentCookie(false);
-    }
-  })
   .catch(err => console.error(err));
 });
