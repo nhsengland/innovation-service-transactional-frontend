102f2d7f4887dc85c50abdf2cbe37c9db8fef411
Jyoti Jaiswal <jyoti.jaiswal@england.nhs.uk>
2021-10-08 21:05:59 +0530

    Password Change message for every role

--- src/modules/feature-modules/assessment/pages/dashboard/dashboard.component.ts
+++ src/modules/feature-modules/assessment/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CoreComponent } from '@app/base';
+import { AlertType } from '@app/base/models';
 
 import { NotificationService } from '@modules/shared/services/notification.service';
 
@@ -10,10 +11,11 @@ import { NotificationService } from '@modules/shared/services/notification.servi
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent extends CoreComponent implements OnInit {
-
+  alert: AlertType = { type: null };
   user: {
     displayName: string;
     organisation: string;
+    passwordResetOn: string;
   };
 
   cardsList: { title: string, link: string, description: string }[];
@@ -28,8 +30,19 @@ export class DashboardComponent extends CoreComponent implements OnInit {
 
     this.user = {
       displayName: this.stores.authentication.getUserInfo().displayName,
-      organisation: 'Needs assessment team'
+      organisation: 'Needs assessment team',
+      passwordResetOn: this.stores.authentication.getUserInfo().passwordResetOn
     };
+
+    const startTime = new Date();
+    const endTime = new Date(this.user.passwordResetOn);
+    const timediffer = startTime.getTime() - endTime.getTime();
+    const resultInMinutes = Math.round(timediffer / 60000);
+
+    if (resultInMinutes <= 1) {
+      this.alert = { type: 'SUCCESS', title: 'You have successfully changed your password', setFocus: true };
+    }
+
 
     this.cardsList = [
       {
