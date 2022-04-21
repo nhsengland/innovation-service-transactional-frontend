a71b7c53526a34dfbd735f629fa69795ee552bed
Ajay Gejage <ajay.gejage@england.nhs.uk>
2022-04-21 18:27:30 +0530

    success message after the change password

--- src/modules/feature-modules/admin/pages/dashboard/dashboard.component.spec.ts
+++ src/modules/feature-modules/admin/pages/dashboard/dashboard.component.spec.ts
@@ -5,16 +5,20 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Injector } from '@angular/core';
 
 import { CoreModule, AppInjector } from '@modules/core';
-import { StoresModule } from '@modules/stores';
+import { AuthenticationService, StoresModule } from '@modules/stores';
 import { AdminModule } from '@modules/feature-modules/admin/admin.module';
 
 import { PageDashboardComponent } from './dashboard.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 
 describe('FeatureModules/Admin/Pages/Dashboard/PageDashboardComponent', () => {
 
   let component: PageDashboardComponent;
   let fixture: ComponentFixture<PageDashboardComponent>;
+  let activatedRoute: ActivatedRoute;
+  let service: AuthenticationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -28,7 +32,8 @@ describe('FeatureModules/Admin/Pages/Dashboard/PageDashboardComponent', () => {
     });
 
     AppInjector.setInjector(TestBed.inject(Injector));
-
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    service = TestBed.inject(AuthenticationService);
   });
 
 
@@ -38,5 +43,20 @@ describe('FeatureModules/Admin/Pages/Dashboard/PageDashboardComponent', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should have alert when password changed', () => {
+    activatedRoute.snapshot.queryParams = { alert: 'xxxx' };
+    service.getUserInfo = () => of({ id: 'id', email: 'john.doe@mail.com', displayName: 'John Doe', type: 'INNOVATOR', roles: [], organisations: [], passwordResetOn: new Date(new Date().getTime() -  1 * 60000).toString(), phone: '' });
+
+    fixture = TestBed.createComponent(PageDashboardComponent);
+    component = fixture.componentInstance;
+
+    const mockAlert = { type: 'SUCCESS', title: 'You have successfully changed your password.', setFocus: true };
+
+    fixture.detectChanges();
+    expect(component.alert).toEqual(mockAlert);
+  });
+
+
 
 });
