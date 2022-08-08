cbfb593918ee02959150f215e0b6a279e3b304d3
Ricardo Tavares <ricardo.tavares@bjss.com>
2022-08-08 10:05:22 +0100

    Try so solve test error only happening during pipeline build

--- src/modules/feature-modules/admin/wizards/organisation-unit-inactivate/organisation-unit-inactivate.component.spec.ts
+++ src/modules/feature-modules/admin/wizards/organisation-unit-inactivate/organisation-unit-inactivate.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 import { Injector } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -11,16 +13,21 @@ import { StoresModule } from '@modules/stores';
 import { AdminModule } from '@modules/feature-modules/admin/admin.module';
 
 import { WizardOrganisationUnitInactivateComponent } from './organisation-unit-inactivate.component';
+import { WizardOrganisationUnitInactivateUsersStepComponent } from './steps/users-step.component';
+import { WizardOrganisationUnitInactivateInnovationsStepComponent } from './steps/innovations-step.component';
+import { WizardSummaryWithConfirmStepComponent } from '@modules/shared/wizards/steps/summary-with-confirm-step.component';
 
 import { OrganisationsService } from '@modules/shared/services/organisations.service';
 
 
-describe('FeatureModules/Admin/Pages/Organisations/PageOrganisationUnitInactivateComponent', () => {
+describe('FeatureModules/Admin/Wizards//WizardOrganisationUnitInactivateComponent', () => {
+
+  // let activatedRoute: ActivatedRoute;
 
   let component: WizardOrganisationUnitInactivateComponent;
   let fixture: ComponentFixture<WizardOrganisationUnitInactivateComponent>;
-  let activatedRoute: ActivatedRoute;
-  let organisationsService: OrganisationsService;
+
+  // let organisationsService: OrganisationsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -31,11 +38,22 @@ describe('FeatureModules/Admin/Pages/Organisations/PageOrganisationUnitInactivat
         StoresModule,
         AdminModule
       ]
+    }).overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [
+          WizardOrganisationUnitInactivateUsersStepComponent,
+          WizardOrganisationUnitInactivateInnovationsStepComponent,
+          WizardSummaryWithConfirmStepComponent
+        ]
+      }
     });
 
     AppInjector.setInjector(TestBed.inject(Injector));
-    activatedRoute = TestBed.inject(ActivatedRoute);
-    organisationsService = TestBed.inject(OrganisationsService);
+
+    // activatedRoute = TestBed.inject(ActivatedRoute);
+
+    // organisationsService = TestBed.inject(OrganisationsService);
+
   });
 
 
