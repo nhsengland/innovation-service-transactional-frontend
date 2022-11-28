aba6530862f02ceaf3f0662f55a79036d7d27e36
Katia <Katia.Duarte@bjss.com>
2022-11-28 15:05:07 +0000

    add recent innovation list

--- src/modules/feature-modules/assessment/pages/dashboard/dashboard.component.ts
+++ src/modules/feature-modules/assessment/pages/dashboard/dashboard.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { CoreComponent } from '@app/base';
+import { TableModel } from '@app/base/models';
+import { DateISOType } from '@app/base/types';
+import { InnovationsListDTO } from '@modules/shared/services/innovations.dtos';
+import { InnovationsListFiltersType, InnovationsService } from '@modules/shared/services/innovations.service';
 import { UserStatisticsTypeEnum } from '@modules/shared/services/statistics.enum';
 import { StatisticsService } from '@modules/shared/services/statistics.service';
+import { forkJoin } from 'rxjs';
 
 type baseStatisticsCard = {
   title: string,
@@ -30,9 +35,12 @@ export class DashboardComponent extends CoreComponent implements OnInit {
 
   cardsList: statisticsSectionsCard[] = [];
 
+  latestInnovations: TableModel<InnovationsListDTO['data'][0], InnovationsListFiltersType>;
+
   constructor(
     private activatedRoute: ActivatedRoute,    
-    private statisticsService: StatisticsService
+    private statisticsService: StatisticsService,    
+    private innovationsService: InnovationsService
   ) {
 
     super();
@@ -45,6 +53,8 @@ export class DashboardComponent extends CoreComponent implements OnInit {
       passwordResetAt: this.stores.authentication.getUserInfo().passwordResetAt || ''
     };
 
+
+    this.latestInnovations = new TableModel({ pageSize: 5 });
   }
 
   ngOnInit(): void {
@@ -60,7 +70,13 @@ export class DashboardComponent extends CoreComponent implements OnInit {
 
     const qp: { statistics: UserStatisticsTypeEnum[] } = { statistics: [UserStatisticsTypeEnum.WAITING_ASSESSMENT_COUNTER, UserStatisticsTypeEnum.ASSIGNED_INNOVATIONS_COUNTER] };
 
-    this.statisticsService.getUserStatisticsInfo(qp).subscribe((statistics) => {
+    forkJoin([
+      this.statisticsService.getUserStatisticsInfo(qp), 
+      this.innovationsService.getInnovationsList(this.latestInnovations.getAPIQueryParams())
+    ]).subscribe(([statistics, innovationsList]) => {
+
+      this.latestInnovations.setData(innovationsList.data, innovationsList.count);
+
       this.cardsList = [{
         title: 'Innovations awaiting assessment',
         label: `Innovations awaiting needs assessment`,
