e3f80507a917434907f7223c9acd579a06ae9ae0
Katia <Katia.Duarte@bjss.com>
2022-12-14 17:59:16 +0000

    update unit test

--- src/modules/feature-modules/innovator/pages/innovation/record/section-edit.component.spec.ts
+++ src/modules/feature-modules/innovator/pages/innovation/record/section-edit.component.spec.ts
@@ -63,7 +63,8 @@ describe('FeatureModules/Innovator/Pages/Innovations/Sections/InnovationSectionE
       section: InnovationSectionEnum.REGULATIONS_AND_STANDARDS,
       status: 'DRAFT' as keyof typeof INNOVATION_SECTION_STATUS,
       updatedAt: '2020-01-01T00:00:00.000Z',
-      data: {}
+      data: {},
+      submittedAt: ''
     });
   });
 
