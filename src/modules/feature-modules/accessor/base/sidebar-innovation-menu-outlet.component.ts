b5600d8356175556897b916a1ad7af9fda869da9
Katia <Katia.Duarte@bjss.com>
2022-12-14 16:15:57 +0000

    add sidebar to accessor

--- src/modules/feature-modules/accessor/base/sidebar-innovation-menu-outlet.component.ts
+++ src/modules/feature-modules/accessor/base/sidebar-innovation-menu-outlet.component.ts
@@ -1,32 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
-import { ContextStore } from '@modules/stores';
-
+import { ContextStore, InnovationStore } from '@modules/stores';
 
 @Component({
   selector: 'app-base-sidebar-innovation-menu-outlet',
   templateUrl: './sidebar-innovation-menu-outlet.component.html'
 })
-export class SidebarInnovationMenuOutletComponent {
+export class SidebarInnovationMenuOutletComponent implements OnDestroy {
+  private subscriptions = new Subscription();
 
-  sidebarItems: { label: string, url: string }[] = [];
-
+  sidebarItems: { label: string, url: string; nestedSidebarItems?: {label: string, url: string;}[] }[] = [];
+  navHeading: string = 'Innovation Record section';
+  showHeading: boolean = false;
 
   constructor(
-    private contextStore: ContextStore
+    private router: Router,
+    private contextStore: ContextStore,
+    private innovationStore: InnovationStore,
   ) {
+    this.subscriptions.add(
+      this.router.events.pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd)).subscribe(e => this.onRouteChange())
+    );
 
-    const innovation = this.contextStore.getInnovation();
-
-    this.sidebarItems = [
-      { label: 'Overview', url: `/accessor/innovations/${innovation.id}/overview` },
-      { label: 'Innovation record', url: `/accessor/innovations/${innovation.id}/record` },
-      { label: 'Action tracker', url: `/accessor/innovations/${innovation.id}/action-tracker` },
-      { label: 'Messages', url: `/accessor/innovations/${innovation.id}/threads` },
-      { label: 'Support status', url: `/accessor/innovations/${innovation.id}/support` },
-      { label: 'Activity log', url: `/accessor/innovations/${innovation.id}/activity-log` }
-    ];
-
+    this.onRouteChange();   
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+  
+  private onRouteChange(): void {
+    const innovation = this.contextStore.getInnovation();
+    this.sidebarItems = [];
+    
+    if (this.router.url.includes('sections')) {
+      const currentSection = this.router.url.split('/').pop();
+      this.showHeading = true;
+      
+      this.innovationStore.getSectionsSummary$(innovation.id).subscribe(response => {
+
+        response.map((parentSection, i) => {
+          this.sidebarItems.push({ label: `${i + 1}. ${parentSection.title}`, url: `/assessment/innovations/${innovation.id}/record/sections/${parentSection.sections[0].id}`, nestedSidebarItems: []  });
+
+          if (parentSection.sections.find(j => j.id === currentSection)) {
+            parentSection.sections.map((section, k) => {
+              this.sidebarItems[i].nestedSidebarItems?.push({ label: `${i + 1}.${k + 1}. ${section.title}`, url: `/assessment/innovations/${innovation.id}/record/sections/${section.id}` });
+            })
+          }
+        });
+      });
+    } else {
+      this.showHeading = false;
+
+      this.sidebarItems = [
+        { label: 'Overview', url: `/accessor/innovations/${innovation.id}/overview` },
+        { label: 'Innovation record', url: `/accessor/innovations/${innovation.id}/record` },
+        { label: 'Action tracker', url: `/accessor/innovations/${innovation.id}/action-tracker` },
+        { label: 'Messages', url: `/accessor/innovations/${innovation.id}/threads` },
+        { label: 'Support status', url: `/accessor/innovations/${innovation.id}/support` },
+        { label: 'Activity log', url: `/accessor/innovations/${innovation.id}/activity-log` }
+      ];
+    }
+  }
 }
