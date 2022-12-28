cf91d91710ec1f8973364223845481df1555180a
Katia <Katia.Duarte@bjss.com>
2022-12-28 16:56:48 +0000

    Refactor sidebar IR creation

--- src/modules/feature-modules/innovator/base/sidebar-innovation-menu-outlet.component.ts
+++ src/modules/feature-modules/innovator/base/sidebar-innovation-menu-outlet.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 
-import { ContextStore, InnovationStore } from '@modules/stores';
-import { debounceTime, filter, Subscription } from 'rxjs';
+import { ContextStore } from '@modules/stores';
+import { getInnovationRecordSidebar, InnovationRecordSidebar } from '@modules/stores/innovation/innovation.config';
+import { filter, Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-base-sidebar-innovation-menu-outlet',
   templateUrl: './sidebar-innovation-menu-outlet.component.html'
 })
-export class SidebarInnovationMenuOutletComponent implements OnDestroy {
+export class SidebarInnovationMenuOutletComponent implements OnInit, OnDestroy {
 
   private subscriptions = new Subscription();
 
   sidebarItems: { label: string, url: string; nestedSidebarItems?: {label: string, url: string;}[] }[] = [];
   navHeading: string = 'Innovation Record sections';
   showHeading: boolean = false;
+  
+  private sectionsSidebar: InnovationRecordSidebar[] = [];
+  private _sidebarItems: { label: string, url: string; }[] = [];
 
   constructor(
     private router: Router,
     private contextStore: ContextStore,
-    private innovationStore: InnovationStore,
   ) {
     this.subscriptions.add(
       this.router.events.pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd)).subscribe(e => {
@@ -28,38 +31,23 @@ export class SidebarInnovationMenuOutletComponent implements OnDestroy {
       })
     );
 
-    this.onRouteChange()
+    this.onRouteChange();   
+  }
+
+  ngOnInit(): void {
+    this.generateSidebar();
   }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
 
+  private generateSidebar(): void {    
+    if (this.sidebarItems.length === 0) {
+      const innovation = this.contextStore.getInnovation();
 
-  private onRouteChange(): void {
-    const innovation = this.contextStore.getInnovation();
-
-    if (this.router.url.includes('sections')) {
-      const currentSection = this.router.url.split('/').pop();
-      this.showHeading = true;
-      this.sidebarItems = [];
-
-      this.innovationStore.getSectionsSummary$(innovation.id).subscribe(response => {
-
-        response.map((parentSection, i) => {
-          this.sidebarItems.push({ label: `${i + 1}. ${parentSection.title}`, url: `/innovator/innovations/${innovation.id}/record/sections/${parentSection.sections[0].id}`, nestedSidebarItems: []  });
-
-          if (parentSection.sections.find(j => j.id === currentSection)) {
-            parentSection.sections.map((section, k) => {
-              this.sidebarItems[i].nestedSidebarItems?.push({ label: `${i + 1}.${k + 1} ${section.title}`, url: `/innovator/innovations/${innovation.id}/record/sections/${section.id}` });
-            })
-          }
-        });
-
-      });
-    } else {
-      this.showHeading = false;
-      this.sidebarItems = [
+      this.sectionsSidebar = getInnovationRecordSidebar('innovator', innovation.id);
+      this._sidebarItems = [
         { label: 'Overview', url: `/innovator/innovations/${innovation.id}/overview` },
         { label: 'Innovation record', url: `/innovator/innovations/${innovation.id}/record` },
         { label: 'Action tracker', url: `/innovator/innovations/${innovation.id}/action-tracker` },
@@ -68,6 +56,17 @@ export class SidebarInnovationMenuOutletComponent implements OnDestroy {
         { label: 'Activity log', url: `/innovator/innovations/${innovation.id}/activity-log` }
       ];
     }
-  
+  }
+
+  private onRouteChange(): void {
+    this.generateSidebar();
+    
+    if (this.router.url.includes('sections')) {
+      this.showHeading = true;
+      this.sidebarItems = this.sectionsSidebar;        
+    } else {
+      this.showHeading = false;
+      this.sidebarItems = this._sidebarItems;
+    }
   }
 }
