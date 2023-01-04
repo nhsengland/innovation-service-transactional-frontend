14ef5837e2c06d84f09d0fc6dac599b46df0717e
Katia <Katia.Duarte@bjss.com>
2023-01-04 12:12:01 +0000

    add storesModule to failing unit tests

--- src/modules/core/interceptors/api-out.interceptor.spec.ts
+++ src/modules/core/interceptors/api-out.interceptor.spec.ts
@@ -8,7 +8,7 @@ import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
 import { ENV, SERVER_REQUEST, SERVER_RESPONSE } from '@tests/app.mocks';
 
 import { CoreModule, EnvironmentVariablesStore } from '@modules/core';
-import { AuthenticationService } from '@modules/stores';
+import { AuthenticationService, AuthenticationStore, StoresModule } from '@modules/stores';
 
 
 describe('Core/Interceptors/ApiOutInterceptor running SERVER side', () => {
@@ -22,7 +22,8 @@ describe('Core/Interceptors/ApiOutInterceptor running SERVER side', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
-        CoreModule
+        CoreModule,
+        StoresModule
       ],
       providers: [
         AuthenticationService,
@@ -75,7 +76,8 @@ describe('Core/Interceptors/ApiOutInterceptor running CLIENT side', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
-        CoreModule
+        CoreModule,
+        StoresModule
       ],
       providers: [
         AuthenticationService,
