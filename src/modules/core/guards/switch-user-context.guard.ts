b209fbc168b130d32719888d12be448d318136d5
Katia <Katia.Duarte@bjss.com>
2023-01-19 10:09:39 +0000

    use isPlatformBrowser instead of window in guard

--- src/modules/core/guards/switch-user-context.guard.ts
+++ src/modules/core/guards/switch-user-context.guard.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, NavigationEnd, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { filter, Observable, of } from 'rxjs';
 
 import { AuthenticationStore } from '@modules/stores/authentication/authentication.store';
 import { LocalStorageHelper } from '@app/base/helpers';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Injectable()
 export class SwitchUserContextGuard implements CanActivate, CanActivateChild {
   notActivate: boolean = false;
 
-  constructor(
+  constructor(    
+    @Inject(PLATFORM_ID) private platformId: object,
     private router: Router,
     private authenticationStore: AuthenticationStore
   ) { 
@@ -20,11 +22,10 @@ export class SwitchUserContextGuard implements CanActivate, CanActivateChild {
       const navigate = (event as NavigationEnd);
       this.notActivate = navigate.url === navigate.urlAfterRedirects;
     });
-
   }
 
   canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): boolean {
-    if (window) {
+    if (isPlatformBrowser(this.platformId)) {
       const url = window.location.pathname;
       return !url.includes('switch-user-context');
     }
