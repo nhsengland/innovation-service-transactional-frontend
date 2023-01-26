22055aa68df8f6475f85bfd67d59061a3e28210a
Miguel.Ferro <miguelferro@LPT-4JPQ5J3.localdomain>
2023-01-26 17:18:27 +0000

    page skeleton

---
+++ src/modules/shared/pages/innovation/data-sharing-and-support/innovation-participants.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CoreComponent } from '@app/base';
+
+import { ContextInnovationType } from '@modules/stores/context/context.types';
+
+
+@Component({
+  selector: 'shared-pages-innovation-participants',
+  templateUrl: './innovation-participants.component.html'
+})
+export class PageInnovationParticipantsComponent extends CoreComponent implements OnInit {
+
+  innovation: ContextInnovationType;
+  
+  constructor(
+  
+  ) {
+
+    super();
+    this.setPageTitle('Everyone who is workig', { showPage: false });
+
+    this.innovation = this.stores.context.getInnovation();
+
+  }
+
+
+  ngOnInit(): void {
+
+    this.setPageStatus('LOADING')
+
+    console.log(this.innovation.id)
+
+    this.setPageStatus('READY')
+  }
+
+}
