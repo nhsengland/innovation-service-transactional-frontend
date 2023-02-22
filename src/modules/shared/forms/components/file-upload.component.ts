a1583f124ee424163534c95705460a5c439d6b35
Katia Duarte <118272002+kduarte-bjss@users.noreply.github.com>
2023-02-22 16:53:17 +0000

    add error message if file is bigger than 9mb (#543)
    * add error message if file is bigger than 9mb
    
    * change alert to error message. Add error message to form engine helper and translation file

--- src/modules/shared/forms/components/file-upload.component.ts
+++ src/modules/shared/forms/components/file-upload.component.ts
@@ -9,6 +9,7 @@ import { RandomGeneratorHelper } from '@modules/core/helpers/random-generator.he
 import { LoggerService, Severity } from '@modules/core/services/logger.service';
 
 import { FileTypes, FileUploadType } from '../engine/types/form-engine.types';
+import { FormEngineHelper } from '@app/base/forms';
 
 
 @Component({
@@ -51,6 +52,8 @@ export class FormFileUploadComponent implements OnInit {
   files: { id: string, file: File }[] = [];
   previousUploadedFiles: FileUploadType[] = [];
 
+  hasError = false;
+  error: { message: string, params: { [key: string]: string } } = { message: '', params: {} };
   isLoadingFile = false;
 
   fileConfig: {
@@ -69,12 +72,20 @@ export class FormFileUploadComponent implements OnInit {
   get fieldArrayControl(): FormArray { return this.parentFieldControl?.get(this.arrayName) as FormArray; }
   get fieldArrayValues(): { id: string, name: string, url: string }[] { return this.fieldArrayControl.value as { id: string, name: string, url: string }[]; }
 
+  // Accessibility.
+  get ariaDescribedBy(): null | string {
+    let s = '';
+    if (this.description) { s += `hint-${this.id}`; }
+    if (this.hasError) { s += `${s ? ' ' : ''}error-${this.id}`; }
+    return s || null;
+  }
+
   constructor(
     private injector: Injector,
     private cdr: ChangeDetectorRef,
     private http: HttpClient,
     private loggerService: LoggerService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
 
@@ -104,13 +115,22 @@ export class FormFileUploadComponent implements OnInit {
   }
 
   onChange(event: NgxDropzoneChangeEvent): void {
-
     if (!this.fileConfig.httpUploadUrl) {
       console.error('No httpUploadUrl provided for file upload.');
     }
 
+    if (event.rejectedFiles.length > 0) {
+      const sizeExceeded = event.rejectedFiles.find((i) => i.reason === 'size');
+
+      if (sizeExceeded) {
+        this.hasError = true;
+        this.error = FormEngineHelper.getValidationMessage({maxFileSize: 'true'});
+      }
+    }
+
     if (event.addedFiles.length > 0) {
       this.isLoadingFile = true;
+      this.hasError = false;
     }
 
     event.addedFiles.forEach(file => {
