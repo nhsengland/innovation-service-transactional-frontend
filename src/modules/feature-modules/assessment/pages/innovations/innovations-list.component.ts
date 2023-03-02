a340feee0018a6fec414488913f79c7680bd3f69
eduarda-meirinhos-bjss <eduarda.meirinhos@bjss.com>
2023-03-02 18:16:34 +0000

    Change ngOnInit to reuse component when params change without reloading

--- src/modules/feature-modules/assessment/pages/innovations/innovations-list.component.ts
+++ src/modules/feature-modules/assessment/pages/innovations/innovations-list.component.ts
@@ -116,42 +116,6 @@ export class InnovationsListComponent extends CoreComponent implements OnInit {
 
     this.availableGroupedStatus = [InnovationGroupedStatusEnum.AWAITING_NEEDS_ASSESSMENT, InnovationGroupedStatusEnum.AWAITING_NEEDS_REASSESSMENT, InnovationGroupedStatusEnum.NEEDS_ASSESSMENT];
 
-    this.subscriptions.push(
-      this.activatedRoute.queryParams
-        .subscribe(({ status }: Params) => {
-
-          let preSelectedStatus: InnovationGroupedStatusEnum[] | undefined;
-
-          switch (status) {
-            case 'COMPLETED':
-              this.showOnlyCompleted = true;
-              this.setPageTitle('Assessment completed innovations');
-              this.setBackLink('Go back', `${this.userUrlBasePath()}/innovations`);
-              this.availableGroupedStatus = [InnovationGroupedStatusEnum.AWAITING_SUPPORT, InnovationGroupedStatusEnum.RECEIVING_SUPPORT, InnovationGroupedStatusEnum.NO_ACTIVE_SUPPORT];
-              break;
-            case 'NEEDS_ASSESSMENT':
-              this.form.get('assignedToMe')?.setValue(true);
-              preSelectedStatus = [InnovationGroupedStatusEnum.NEEDS_ASSESSMENT];
-              break;
-            case 'WAITING_NEEDS_ASSESSMENT':
-              preSelectedStatus = [InnovationGroupedStatusEnum.AWAITING_NEEDS_ASSESSMENT, InnovationGroupedStatusEnum.AWAITING_NEEDS_REASSESSMENT];
-              break;
-            default:
-          }
-
-          if (preSelectedStatus) {
-            preSelectedStatus.forEach(status => (this.form.get('groupedStatuses') as FormArray).push(new FormControl(status)));
-
-            const groupedStatusesFilter = this.filters.find(f => f.key === 'groupedStatuses');
-            if (groupedStatusesFilter) {
-              groupedStatusesFilter.showHideStatus = 'opened';
-            }
-          }
-
-
-        })
-    );
-
     this.innovationsList.setVisibleColumns({
       name: { label: 'Innovation', orderable: true },
       submittedAt: { label: 'Submitted', orderable: true },
@@ -164,6 +128,54 @@ export class InnovationsListComponent extends CoreComponent implements OnInit {
 
   ngOnInit(): void {
 
+
+    this.setDatasetGroupedStatuses();
+
+    this.subscriptions.push(
+
+      this.activatedRoute.queryParams
+      .subscribe(({ status }: Params) => {
+
+        let preSelectedStatus: InnovationGroupedStatusEnum[] | undefined;
+
+        switch (status) {
+          case 'COMPLETED':
+            this.showOnlyCompleted = true;
+            this.setPageTitle('Assessment completed innovations');
+            this.setBackLink('Go back', `${this.userUrlBasePath()}/innovations`);
+            this.availableGroupedStatus = [InnovationGroupedStatusEnum.AWAITING_SUPPORT, InnovationGroupedStatusEnum.RECEIVING_SUPPORT, InnovationGroupedStatusEnum.NO_ACTIVE_SUPPORT];
+            this.setDatasetGroupedStatuses();
+            break;
+          case 'NEEDS_ASSESSMENT':
+            this.form.get('assignedToMe')?.setValue(true);
+            preSelectedStatus = [InnovationGroupedStatusEnum.NEEDS_ASSESSMENT];
+            break;
+          case 'WAITING_NEEDS_ASSESSMENT':
+            preSelectedStatus = [InnovationGroupedStatusEnum.AWAITING_NEEDS_ASSESSMENT, InnovationGroupedStatusEnum.AWAITING_NEEDS_REASSESSMENT];
+            break;
+          default:
+        }
+
+        if (preSelectedStatus) {
+          preSelectedStatus.forEach(status => (this.form.get('groupedStatuses') as FormArray).push(new FormControl(status)));
+
+          const groupedStatusesFilter = this.filters.find(f => f.key === 'groupedStatuses');
+          if (groupedStatusesFilter) {
+            groupedStatusesFilter.showHideStatus = 'opened';
+          }
+        }
+
+        this.onFormChange();
+
+      }),
+
+      this.form.valueChanges.pipe(debounceTime(500)).subscribe(() => this.onFormChange())
+
+    );
+
+  }
+
+  setDatasetGroupedStatuses(): void {
     const descriptions = new Map([
       [InnovationGroupedStatusEnum.AWAITING_SUPPORT, 'Waiting for an organisation unit to start supporting this innovation.'],
       [InnovationGroupedStatusEnum.RECEIVING_SUPPORT, 'At least one organisation is supporting the innovation.'],
@@ -171,14 +183,7 @@ export class InnovationsListComponent extends CoreComponent implements OnInit {
     ]);
 
     this.datasets.groupedStatuses = this.availableGroupedStatus
-      .map(groupedStatus => ({ label: this.translate(`shared.catalog.innovation.grouped_status.${groupedStatus}.name`), value: groupedStatus, description: descriptions.get(groupedStatus) }))
-
-    this.subscriptions.push(
-      this.form.valueChanges.pipe(debounceTime(500)).subscribe(() => this.onFormChange())
-    );
-
-    this.onFormChange();
-
+              .map(groupedStatus => ({ label: this.translate(`shared.catalog.innovation.grouped_status.${groupedStatus}.name`), value: groupedStatus, description: descriptions.get(groupedStatus) }));
   }
 
   getInnovationsList(): void {
