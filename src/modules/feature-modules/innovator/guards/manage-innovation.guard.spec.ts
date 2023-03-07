4665c9ee93001ff3f8da4978088fe1e6ac68fb0e
eduarda-meirinhos-bjss <eduarda.meirinhos@bjss.com>
2023-03-07 13:58:02 +0000

    Add imports to ManageInnovationGuard spec file

--- src/modules/feature-modules/innovator/guards/manage-innovation.guard.spec.ts
+++ src/modules/feature-modules/innovator/guards/manage-innovation.guard.spec.ts
@@ -1,18 +1,35 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CoreModule } from '@modules/core';
+import { InnovationsService } from '@modules/shared/services/innovations.service';
+import { AuthenticationStore, StoresModule } from '@modules/stores';
 
 import { ManageInnovationGuard } from './manage-innovation.guard';
 
 describe('ManageInnovationGuard', () => {
+
   let guard: ManageInnovationGuard;
+  let authenticationStore: AuthenticationStore;
+  let innovationsService: InnovationsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        CoreModule,
+        StoresModule
+      ],
       providers: [
         ManageInnovationGuard
       ]
     });
 
     guard = TestBed.inject(ManageInnovationGuard);
+    authenticationStore = TestBed.inject(AuthenticationStore);
+    innovationsService = TestBed.inject(InnovationsService);
+
   });
 
   it('should be created', () => {
