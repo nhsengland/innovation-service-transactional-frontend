769212e08537327e5ae74541bae08c9630103dee
Katia <Katia.Duarte@bjss.com>
2023-03-07 13:13:05 +0000

    update manage transfer logic

--- src/modules/feature-modules/innovator/pages/innovation/manage/manage-transfer.component.ts
+++ src/modules/feature-modules/innovator/pages/innovation/manage/manage-transfer.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 
 import { CoreComponent } from '@app/base';
-import { CustomValidators, FormGroup, FormEngineParameterModel, Validators } from '@app/base/forms';
+import { CustomValidators, FormGroup, FormEngineParameterModel, Validators, WizardEngineModel, FormEngineComponent } from '@app/base/forms';
 
 import { InnovatorService } from '@modules/feature-modules/innovator/services/innovator.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormEngineModel, WizardSummaryType } from '@modules/shared/forms';
+import { COLLABORATORS_TRANSFERS, NO_COLLABORATORS_TRANSFERS } from './manage-transfer.config';
 
 
 @Component({
@@ -13,16 +15,21 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './manage-transfer.component.html'
 })
 export class PageInnovationManageTransferComponent extends CoreComponent implements OnInit {
+  
+  @ViewChild(FormEngineComponent) formEngineComponent?: FormEngineComponent;
 
   innovationId: string;
+  stayAsCollaborator: boolean = false;
+
+  wizard: WizardEngineModel = new WizardEngineModel({});
+  summaryList: WizardSummaryType[] = [];
+
+  isQuestionStep(): boolean { return Number.isInteger(Number(this.activatedRoute.snapshot.params.stepId)); }
+  isSummaryStep(): boolean { return this.activatedRoute.snapshot.params.stepId === 'summary'; }
 
   form = new FormGroup({
-    email: new UntypedFormControl('', [CustomValidators.required('An email is required'), Validators.email]),
     confirmation: new UntypedFormControl('', [CustomValidators.required('A confirmation text is necessary'), CustomValidators.equalTo('transfer my innovation')]),
   }, { updateOn: 'blur' });
-
-  formInnovationsItems: FormEngineParameterModel['items'] = [];
-
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -30,28 +37,79 @@ export class PageInnovationManageTransferComponent extends CoreComponent impleme
   ) {
 
     super();
-    this.setPageTitle('Transfer ownership of this innovation');
 
     this.innovationId = this.activatedRoute.snapshot.params.innovationId;
-
   }
 
   ngOnInit(): void {
-    this.setPageStatus('READY');
+    this.wizard = NO_COLLABORATORS_TRANSFERS;
+
+    this.innovatorService.getInnovationCollaborationInfo(this.innovationId).subscribe(response => {
+      if (response.count === 0) {
+        this.wizard = NO_COLLABORATORS_TRANSFERS;
+      } else {
+        this.wizard = COLLABORATORS_TRANSFERS;
+        //add collaborators
+        this.wizard.steps[0].parameters[0].items?.unshift({
+          value: 'emasd@aa.com',
+          label: 'Test'  
+        });
+
+        // Updates wizard configuration step 1 description.
+        this.wizard.setAnswers(this.wizard.runInboundParsing({})).runRules();
+      }
+
+      this.subscriptions.push(
+        this.activatedRoute.params.subscribe(params => {
+
+          if (!this.wizard.isValidStep(params.stepId)) {
+            this.redirectTo('/not-found');
+            return;
+          }
+
+          if (this.isSummaryStep()) {
+            const wizardData = this.wizard.runOutboundParsing();
+            this.stayAsCollaborator = wizardData.isCollaborator;
+            this.setPageTitle(`Transfer ownership of this innovation to ${wizardData.email} ${this.stayAsCollaborator ? 'but continue to collaborate on it' : ''}`, { size: 'l' });
+
+            this.setBackLink('Go back', this.onSubmitStep.bind(this, 'previous', new Event('')));
+            this.setPageStatus('READY');
+            return;
+          }
+
+          this.wizard.gotoStep(Number(params.stepId));
+          this.setPageTitle(this.wizard.currentStepTitle() || '');
+          this.setBackLink('Go back', this.onSubmitStep.bind(this, 'previous', new Event('')));         
+          this.setPageStatus('READY');
+
+        })
+      );
+
+    });
   }
 
-  onSubmitForm(): void {
+  onSubmitStep(action: 'previous' | 'next', event: Event): void {
+    event.preventDefault();
 
-    if (!this.form.valid) {
-      this.form.markAllAsTouched();
+    const formData = this.formEngineComponent?.getFormValues();
+
+    if (action === 'next' && !formData?.valid) { // Apply validation only when moving forward.
       return;
     }
 
-    const body: { innovationId: string, email: string } = {
-      innovationId: this.innovationId!,
-      email: this.form.get('email')!.value
-    };
+    this.wizard.addAnswers(formData?.data || {}).runRules();
 
+    this.redirectTo(this.getNavigationUrl(action));
+  }
+
+  onSubmitWizard(): void {
+    const wizardData = this.wizard.runOutboundParsing();
+
+    const body: { innovationId: string, email: string, isCollaborator: boolean } = {
+      innovationId: this.innovationId,
+      email: wizardData.value,
+      isCollaborator: wizardData.isCollaborator
+    };
 
     this.innovatorService.transferInnovation(body).subscribe({
       next: () => {
@@ -61,7 +119,29 @@ export class PageInnovationManageTransferComponent extends CoreComponent impleme
         this.setAlertError('An error occurred when transferring innovation ownership. Please check the details and try again or contact us for further info.');
       }
     });
-
   }
+  
+  private getNavigationUrl(action: 'previous' | 'next'): string {
+    let url = `/innovator/innovations/${this.innovationId}/manage-innovation`;
 
+    switch (action) {
+      case 'previous':
+        if (this.wizard.isFirstStep()) { url += ``; }
+        else if (this.isSummaryStep()) { url += `/transfer/${this.wizard.steps.length}`; }
+        else { url += `/transfer/${Number(this.wizard.currentStepId) - 1}`; }
+        break;
+
+      case 'next':
+        if (this.isSummaryStep()) { url += ``; }
+        else if (this.wizard.isLastStep()) { url += `/transfer/summary`; }
+        else { url += `/transfer/${Number(this.wizard.currentStepId) + 1}`; }
+        break;
+
+      default: // Should NOT happen!
+        url += '';
+        break;
+    }
+    
+    return url;
+  }
 }
