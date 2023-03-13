1eacb28cefb021c03f3cbb2f1088b45ea04021b1
Katia <Katia.Duarte@bjss.com>
2023-03-13 16:08:38 +0000

    Add unit info endpoint and to UI

--- src/modules/feature-modules/admin/pages/organisations/organisation-unit-info/organisation-unit-info.component.ts
+++ src/modules/feature-modules/admin/pages/organisations/organisation-unit-info/organisation-unit-info.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CoreComponent } from '@app/base';
+import { GetOrganisationUnitInfoDTO, OrganisationsService } from '@modules/shared/services/organisations.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-admin-pages-organisations-unit-info',
   templateUrl: './organisation-unit-info.component.html'
 })
 export class PageOrganisationUnitInfoComponent extends CoreComponent implements OnInit {
+  organisationId: string;
+  organisationUnitId: string;
 
-  constructor() { 
+  unit: GetOrganisationUnitInfoDTO = { id: '', name: '', acronym: '', isActive: false, userCount: 0};
+
+  constructor(    
+    private activatedRoute: ActivatedRoute,
+    private organisationsService: OrganisationsService
+  ) { 
     super();
     this.setPageTitle('Unit Information');
+
+    
+    this.organisationId = this.activatedRoute.snapshot.params.organisationId;
+    this.organisationUnitId = this.activatedRoute.snapshot.params.organisationUnitId;
   }
 
   ngOnInit(): void {
+    forkJoin([
+      this.organisationsService.getOrganisationUnitInfo(this.organisationId, this.organisationUnitId),
+      this.organisationsService.getOrganisationUnitUsersList(this.organisationUnitId, {})
+    ]).subscribe({
+
+      next: ([unitInfo, unitUsers]) => {
+        this.unit = unitInfo;
+        this.setPageStatus('READY');
+      },
+      error: () => {
+        this.setPageStatus('READY');
+      }
+      
+    });
   }
 
 }
