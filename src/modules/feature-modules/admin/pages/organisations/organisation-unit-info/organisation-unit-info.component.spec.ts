5d72d2314625d4c465fb4f6b189d9e5ee25e6401
Katia <Katia.Duarte@bjss.com>
2023-03-15 10:16:32 +0000

    update unit test

--- src/modules/feature-modules/admin/pages/organisations/organisation-unit-info/organisation-unit-info.component.spec.ts
+++ src/modules/feature-modules/admin/pages/organisations/organisation-unit-info/organisation-unit-info.component.spec.ts
@@ -1,23 +1,48 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Injector } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppInjector, CoreModule } from '@modules/core';
+import { AdminModule } from '@modules/feature-modules/admin/admin.module';
+import { InnovationsService } from '@modules/shared/services/innovations.service';
+import { OrganisationsService } from '@modules/shared/services/organisations.service';
+import { StoresModule } from '@modules/stores';
 
 import { PageOrganisationUnitInfoComponent } from './organisation-unit-info.component';
 
-describe('PageOrganisationUnitInfoComponent', () => {
+describe('FeatureModules/Admin/Pages/Organisations/PageOrganisationUnitInfoComponent', () => {
+  let activatedRoute: ActivatedRoute;
+
+  let organisationsService: OrganisationsService;
+  let innovationsService: InnovationsService;
+
   let component: PageOrganisationUnitInfoComponent;
   let fixture: ComponentFixture<PageOrganisationUnitInfoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PageOrganisationUnitInfoComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({    
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        CoreModule,
+        StoresModule,
+        AdminModule
+      ]
+    });
+    
+    AppInjector.setInjector(TestBed.inject(Injector));
 
-    fixture = TestBed.createComponent(PageOrganisationUnitInfoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    activatedRoute = TestBed.inject(ActivatedRoute);
+
+    organisationsService = TestBed.inject(OrganisationsService);
+    innovationsService = TestBed.inject(InnovationsService);
   });
 
-  it('should create', () => {
+
+  it('should create the component', () => {
+    fixture = TestBed.createComponent(PageOrganisationUnitInfoComponent);
+    component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
