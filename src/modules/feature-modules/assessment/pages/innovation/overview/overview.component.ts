15848f66b3d14314a0ff8233edf15ea5cd0a4eda
Katia Duarte <118272002+kduarte-bjss@users.noreply.github.com>
2023-04-18 12:36:14 +0100

    Add [deleted account] to owner. Update checkbox size (#701)

--- src/modules/feature-modules/assessment/pages/innovation/overview/overview.component.ts
+++ src/modules/feature-modules/assessment/pages/innovation/overview/overview.component.ts
@@ -68,7 +68,7 @@ export class InnovationOverviewComponent extends CoreComponent implements OnInit
       ];
 
       this.innovatorSummary = [
-        { label: 'Owner', value: this.innovation.owner?.name ?? '' },
+        { label: 'Owner', value: this.innovation.owner?.name ?? '[deleted account]' },
         { label: 'Contact preference', value: UtilsHelper.getContactPreferenceValue(this.innovation.owner?.contactByEmail, this.innovation.owner?.contactByPhone, this.innovation.owner?.contactByPhoneTimeframe) || '' },
         { label: 'Contact details', value: this.innovation.owner?.contactDetails || '' },
         { label: 'Email address', value: this.innovation.owner?.email || '' },
