0ce6d71d8c158e15999cd2cf392a17c27c8fd9a9
eduarda-meirinhos-bjss <124596230+eduarda-meirinhos-bjss@users.noreply.github.com>
2023-04-24 12:28:27 +0100

    Update switch profile redirect alert message everytime user switches profile (#728)

--- src/modules/shared/pages/switch-context/switch-context.component.ts
+++ src/modules/shared/pages/switch-context/switch-context.component.ts
@@ -90,10 +90,8 @@ export class PageSwitchContextComponent extends CoreComponent {
         organisationUnit: role.organisationUnit
       });
 
-      if (!this.currentRole) {
-        const roleDescription = `${this.authenticationStore.getRoleDescription(role.type).toLowerCase()}${role.organisationUnit ? ` (${role.organisationUnit.name})` : ''}`;
-        this.setRedirectAlertSuccess(`You are now logged in as ${roleDescription}.`);
-      }
+      const roleDescription = `${this.authenticationStore.getRoleDescription(role.type).toLowerCase()}${role.organisationUnit ? ` (${role.organisationUnit.name})` : ''}`;
+      this.setRedirectAlertSuccess(`You are now logged in as ${roleDescription}.`);
 
       this.stores.authentication.initializeAuthentication$().subscribe(() => {
         if (this.stores.authentication.hasAnnouncements()) {
