7eefd6385a2c763ddd37e918c02334d5f8fb5595
Miguel-Ferro-bjss <miguel.ferro@bjss.com>
2023-05-16 15:42:35 +0100

    reverse bad change

--- src/modules/feature-modules/innovator/base/sidebar-account-menu-outlet.component.ts
+++ src/modules/feature-modules/innovator/base/sidebar-account-menu-outlet.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class SidebarAccountMenuOutletComponent {
 
-  disableCreateButton = false;
   sidebarItems: { label: string, url: string }[] = [];
 
 
