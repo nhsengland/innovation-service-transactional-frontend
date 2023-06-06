e6efc8dc3148c42f4548cb7ea527baea89197a03
Manuel Sousa <manuel.sousa@bjss.com>
2023-06-06 10:43:56 +0100

    reload page on 401 for b2c recovery

--- src/modules/core/interceptors/api-in.interceptor.ts
+++ src/modules/core/interceptors/api-in.interceptor.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { EMPTY, Observable, catchError, throwError } from 'rxjs';
 
 
 @Injectable()
 export class ApiInInterceptor implements HttpInterceptor {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -15,7 +18,13 @@ export class ApiInInterceptor implements HttpInterceptor {
         // console.log('APIin', error);
 
         if (error.status === 401) {
-          return next.handle(request.clone());
+          // If 401 and browser reload the page to fetch a new token from the identity provider.
+          if (isPlatformBrowser(this.platformId)) {
+            location.reload();
+            return EMPTY;
+          } else {
+            return next.handle(request.clone());
+          }
         } else {
 
           // If error is handled on the subscription itself, this error will be ignored!
