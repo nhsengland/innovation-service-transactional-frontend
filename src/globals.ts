c6cf37ef6b383ffd2b48fd167528760f670ce41c
Manuel Sousa <manuel.sousa@bjss.com>
2023-07-14 12:19:48 +0100

    appinsights testing user injection

--- src/globals.ts
+++ src/globals.ts
@@ -9,6 +9,16 @@ export const initAppInsights = () => {
       .setup(process.env.APPINSIGHTS_INSTRUMENTATION_KEY)
       .setDistributedTracingMode(appinsights.DistributedTracingModes.AI_AND_W3C)
       .start();
+
+    appinsights.defaultClient.addTelemetryProcessor((envelope, context) => {
+      envelope.tags['xpto'] = 'xpto'; // TODO remove
+      const oid = context?.['http.ServerRequest']?.session?.oid;
+      if(oid) {
+        context.data.authenticatedUser = oid;
+        context.data.session = context?.['http.ServerRequest']?.sessionID;
+      }
+      return true;
+    });
   }
 
   return appinsights;
