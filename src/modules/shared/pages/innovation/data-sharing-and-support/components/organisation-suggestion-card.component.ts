3be9a8de05a37b98727cd0029e1234e2b6d9bf0d
Pablo Codeglia <codeglia.pablo@gmail.com>
2023-09-26 10:01:46 +0100

    Task/150753 content change on suggest org card (#937)
    * merged everything in 1 card
    
    * added sorting to NA suggestions
    
    * renamed method back to original name
    
    * changed title back to Dev's
    
    * changed title back to Dev's
    
    * removed imports
    
    * moved filter up, in parseAccessors

--- src/modules/shared/pages/innovation/data-sharing-and-support/components/organisation-suggestion-card.component.ts
+++ src/modules/shared/pages/innovation/data-sharing-and-support/components/organisation-suggestion-card.component.ts
@@ -18,67 +18,56 @@ export class OrganisationSuggestionsCardComponent implements OnChanges {
     organisations: string[]
   };
 
-  accessors: {
-    suggestors: string,
-    organisations: string[]
-  };
+  accessors: AccessorSuggestionModel[] | undefined;
 
-  showAssessmentsCard: boolean;
-  showAccessorsCard: boolean;
+  showAssessments: boolean;
+  showAccessors: boolean;
 
   // hasNewSuggestions = false;
 
   constructor(
     // private notificationsService: NotificationsService,
   ) {
-    this.showAccessorsCard = false;
-    this.showAssessmentsCard = false;
+    this.showAccessors = false;
+    this.showAssessments = false;
 
     this.assessments = {
       organisations: [],
     };
 
-    this.accessors = {
-      suggestors: '',
-      organisations: []
-    };
+    this.accessors = [];
+    ;
   }
 
   ngOnChanges(): void {
     if (this.suggestions) {
-      this.accessors = this.parseAccessors(this.suggestions.accessors ?? []);
       this.assessments = this.parseAssessments(this.suggestions.assessment);
-
       if (this.assessments && this.assessments.organisations.length > 0) {
-        this.showAssessmentsCard = true;
-      }
+        this.showAssessments = true;
+      };
 
-      if (this.accessors && this.accessors.organisations.length > 0) {
-        this.showAccessorsCard = true;
-      }
+      this.accessors = this.parseAccessors(this.suggestions.accessors);
+      if(this.accessors && this.accessors.length > 0){
+        this.showAccessors = true
+      };
     }
 
     // this.hasNewSuggestions = this.notificationsService.notifications[NotificationContextTypeEnum.DATA_SHARING] ? true : false;
   }
 
-  private parseAccessors(accessorsSuggestions: AccessorSuggestionModel[]): { suggestors: string, organisations: string[] } {
+  private parseAccessors(accessorsSuggestions: AccessorSuggestionModel[]): AccessorSuggestionModel[] {
     const shares = new Set(this.shares?.map(s => s.organisationId) || []);
-    const accessorsUnits = accessorsSuggestions.map(as => `${as.organisationUnit.name} (${as.organisationUnit.organisation.acronym})`);
-    const suggestedOrganisations = accessorsSuggestions
-      .flatMap(as => as.suggestedOrganisationUnits
-        .map(ou => ou.organisation)
-        .filter(so => !shares.has(so.id))
-        .map(so => `${so.name} (${so.acronym})`)
-      );
+    
+    let filteredSuggestions: AccessorSuggestionModel[] = accessorsSuggestions.map((element) => {
+      return {
+      ...element, suggestedOrganisationUnits: element.suggestedOrganisationUnits.filter(org => !shares.has(org.organisation.id))
+        .sort((a,b) => a.name.localeCompare(b.name))
+      }
+    });
 
-    // removes duplicate entries
-    const organisations = [...new Set(suggestedOrganisations)];
-    const accessors = [... new Set(accessorsUnits)];
-    return {
-      suggestors: accessors.join(', '),
-      organisations,
-    };
+    return filteredSuggestions;
   }
+
 
   private parseAssessments(assessmentsSuggestions: AssessmentSuggestionModel): { organisations: string[] } {
 
@@ -91,6 +80,8 @@ export class OrganisationSuggestionsCardComponent implements OnChanges {
     // removes duplicate entries
     const organisations = [...new Set(suggestedOrganisations)];
 
+    organisations.sort((a,b)=>a.localeCompare(b));
+    
     return {
       organisations,
     };
