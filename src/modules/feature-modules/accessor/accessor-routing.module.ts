fb6cb753638642fb97c4fbc818b4cfbf235f4ce5
Pablo Codeglia <codeglia.pablo@gmail.com>
2023-10-04 10:49:29 +0100

    Task/151294 update assign new support status flow (#974)
    * added count to test
    
    * created service for support status / changed page
    
    * updated change support status button to alink when not unassigned
    
    * go back changes / minor fixs
    
    * removed wrong text
    
    * removed console.log
    
    * changed p to ul/li

--- src/modules/feature-modules/accessor/accessor-routing.module.ts
+++ src/modules/feature-modules/accessor/accessor-routing.module.ts
@@ -339,11 +339,14 @@ const routes: Routes = [
                   { path: 'statuses', pathMatch: 'full', component: PageInnovationSupportStatusListComponent,
                     data: { breadcrumb: 'Statuses' }
                   },
-                  { path: 'new', pathMatch: 'full', component: InnovationSupportUpdateComponent },
+                  { path: 'new', pathMatch: 'full', component: InnovationSupportUpdateComponent,
+                    data: { layout: { type: 'full' } }  
+                  },
                   { path: 'suggest', pathMatch: 'full', component: InnovationSupportOrganisationsSupportStatusSuggestComponent },
-                  { path: ':supportId', pathMatch: 'full', component: InnovationSupportUpdateComponent },
-                  {
-                    path: ':supportId/request-update', pathMatch: 'full', component: InnovationSupportRequestUpdateStatusComponent,
+                  { path: ':supportId', pathMatch: 'full', component: InnovationSupportUpdateComponent,
+                    data: { layout: { type: 'full' } }
+                  },
+                  { path: ':supportId/request-update', pathMatch: 'full', component: InnovationSupportRequestUpdateStatusComponent,
                     data: { layout: { type: 'full' } }
                   }
                 ]
