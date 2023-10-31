783508fd960938ad5db30080975978347a6e24f5
Manuel Sousa - BJSS <115169817+manuelsousa-bjss@users.noreply.github.com>
2023-10-31 12:14:23 +0000

    fix organisation shares warning ignoring currently shared (#1023)

--- src/modules/shared/pages/innovation/data-sharing-and-support/components/organisation-suggestion-card.component.ts
+++ src/modules/shared/pages/innovation/data-sharing-and-support/components/organisation-suggestion-card.component.ts
@@ -35,13 +35,19 @@ export class OrganisationSuggestionsCardComponent implements OnChanges {
   }
 
   ngOnChanges(): void {
+    this.showAccessors = false;
+    this.showAssessments = false;
+    const sharesSet = new Set(this.shares?.map(item => item.organisationId));
     if (this.suggestions) {
-      this.assessments.organisations = this.suggestions.assessment.suggestedOrganisations.map(i => i.name);
+      this.assessments.organisations = this.suggestions.assessment.suggestedOrganisations.filter(i => !sharesSet.has(i.id)).map(i => i.name);
       if (this.assessments && this.assessments.organisations.length > 0) {
         this.showAssessments = true;
       };
 
-      this.accessors = this.suggestions.accessors;
+      this.accessors = this.suggestions.accessors.map(a => ({
+        organisation: a.organisation,
+        suggestedOrganisations: a.suggestedOrganisations.filter(i => !sharesSet.has(i.id))
+      })).filter(a => a.suggestedOrganisations.length > 0);
       if(this.accessors && this.accessors.length > 0){
         this.showAccessors = true
       };
