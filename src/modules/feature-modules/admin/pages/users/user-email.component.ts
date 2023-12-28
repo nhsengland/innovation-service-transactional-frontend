50516756bea784c26b442caa00aa8bcbbca40749
Diogo Costa <diogo.costa@bjss.com>
2023-12-28 16:59:39 +0000

    feat: minor changes and improvements
    - Added go back
    - Show all errors when form is not valid
    - Remove uneeded onInit cycle

--- src/modules/feature-modules/admin/pages/users/user-email.component.ts
+++ src/modules/feature-modules/admin/pages/users/user-email.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { CoreComponent } from '@app/base';
@@ -12,7 +12,7 @@ import { AdminUsersService } from '../../services/users.service';
   selector: 'app-admin-pages-users-user-email',
   templateUrl: './user-email.component.html'
 })
-export class PageUserEmailComponent extends CoreComponent implements OnInit {
+export class PageUserEmailComponent extends CoreComponent {
   user: { id: string; name: string };
   form = new FormGroup(
     {
@@ -36,30 +36,31 @@ export class PageUserEmailComponent extends CoreComponent implements OnInit {
       name: RoutingHelper.getRouteData<any>(this.activatedRoute).user.displayName
     };
 
+    this.setBackLink('Go back');
     this.setPageTitle('Change user email', { hint: this.user.name });
-  }
-
-  ngOnInit(): void {
     this.setPageStatus('READY');
   }
 
   onSubmit(): void {
-    if (this.form.valid) {
-      const email = this.form.get('email')?.value;
-      if (email) {
-        this.usersService.changeUserEmail(this.user.id, email).subscribe({
-          next: () => this.redirectTo(`/admin/users/${this.user.id}`, { alert: 'changeEmailSuccess' }),
-          error: res => {
-            if (res.status === 409) {
-              this.form.get('email')?.setErrors({ conflict: true });
-              this.setAlertError('User email already exists');
-            } else {
-              this.setPageStatus('ERROR');
-              this.setAlertUnknownError();
-            }
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const email = this.form.get('email')?.value;
+    if (email) {
+      this.usersService.changeUserEmail(this.user.id, email).subscribe({
+        next: () => this.redirectTo(`/admin/users/${this.user.id}`, { alert: 'changeEmailSuccess' }),
+        error: res => {
+          if (res.status === 409) {
+            this.form.get('email')?.setErrors({ conflict: true });
+            this.setAlertError('User email already exists');
+          } else {
+            this.setPageStatus('ERROR');
+            this.setAlertUnknownError();
           }
-        });
-      }
+        }
+      });
     }
   }
 }
