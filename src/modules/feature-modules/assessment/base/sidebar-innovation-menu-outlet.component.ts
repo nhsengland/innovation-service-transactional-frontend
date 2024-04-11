6436e7a470a007e745a6cc736ba8c8a0b8045c9c
pablocodeglia <pablo.codeglia@bjss.com>
2024-04-11 16:55:35 +0100

    refactor logic to display 'support summary' link

--- src/modules/feature-modules/assessment/base/sidebar-innovation-menu-outlet.component.ts
+++ src/modules/feature-modules/assessment/base/sidebar-innovation-menu-outlet.component.ts
@@ -52,6 +52,8 @@ export class SidebarInnovationMenuOutletComponent implements OnInit, OnDestroy {
     if (this.sidebarItems.length === 0) {
       const innovation = this.contextStore.getInnovation();
 
+      const shouldShowSupportSummary: boolean = false;
+
       this.sectionsSidebar = this.innovationStore.getInnovationRecordSectionsTree('assessment', innovation.id);
       this._sidebarItems = [
         { label: 'Overview', url: `/assessment/innovations/${innovation.id}/overview` },
@@ -61,13 +63,14 @@ export class SidebarInnovationMenuOutletComponent implements OnInit, OnDestroy {
         ...(innovation.status !== InnovationStatusEnum.CREATED
           ? [{ label: 'Documents', url: `/assessment/innovations/${innovation.id}/documents` }]
           : []),
-        ...([
-          InnovationStatusEnum.IN_PROGRESS,
-          InnovationStatusEnum.AWAITING_NEEDS_REASSESSMENT,
-          InnovationStatusEnum.ARCHIVED
-        ].includes(innovation.status)
-          ? [{ label: 'Support summary', url: `/assessment/innovations/${innovation.id}/support-summary` }]
-          : []),
+        ...((innovation.assessment?.finishedAt == null &&
+          innovation.reassessmentCount === 0 &&
+          innovation.status !== 'ARCHIVED') ||
+        (innovation.status === 'ARCHIVED' &&
+          innovation.archivedStatus !== 'IN_PROGRESS' &&
+          innovation.reassessmentCount === 0)
+          ? []
+          : [{ label: 'Support summary', url: `/assessment/innovations/${innovation.id}/support-summary` }]),
         { label: 'Data sharing preferences', url: `/assessment/innovations/${innovation.id}/support` },
         { label: 'Activity log', url: `/assessment/innovations/${innovation.id}/activity-log` }
       ];
