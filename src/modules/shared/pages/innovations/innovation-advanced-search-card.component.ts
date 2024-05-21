e621cad9449864e62f908df471eb6975c96d93a0
eduarda-meirinhos-bjss <eduarda.meirinhos@bjss.com>
2024-05-21 15:45:55 +0100

    Update advanced search cards to display search result information

--- src/modules/shared/pages/innovations/innovation-advanced-search-card.component.ts
+++ src/modules/shared/pages/innovations/innovation-advanced-search-card.component.ts
@@ -27,6 +27,7 @@ export type InnovationCardData = {
     updatedAt: DateISOType | null;
     closedReason: InnovationStatusEnum.ARCHIVED | 'STOPPED_SHARED' | InnovationSupportStatusEnum.CLOSED | null;
   } | null;
+  highlights?: Record<string, string[]>;
 };
 
 @Component({
@@ -34,23 +35,30 @@ export type InnovationCardData = {
   templateUrl: './innovation-advanced-search-card.component.html'
 })
 export class InnovationAdvancedSearchCardComponent extends CoreComponent implements OnInit {
+  @Input({ required: true }) innovationCardData!: InnovationCardData;
+
   baseUrl: string;
 
   isAdminType: boolean;
   isAccessorType: boolean;
+  isAssessmentType: boolean;
 
   isInnovationInArchivedStatus: boolean = false;
 
   isAccessorTypeAndArchivedInnovation: boolean = false;
   isAccessorTypeAndStoppedSharingInnovation: boolean = false;
 
-  @Input({ required: true }) innovationCardData!: InnovationCardData;
-
   categoriesList: string = '';
   careSettingsList: string = '';
   diseasesAndConditionsList: string = '';
   keyHealthInequalitiesList: string = '';
   involvedAACProgrammesList: string = '';
+
+  searchTermsFound: string[] = [];
+  searchTermsCount = new Map<string, number>();
+  snippet: string = '';
+
+  goToSectionLink = { text: '', link: '', queryParams: { search: '' } };
 
   constructor() {
     super();
@@ -59,7 +67,9 @@ export class InnovationAdvancedSearchCardComponent extends CoreComponent impleme
 
     this.isAdminType = this.stores.authentication.isAdminRole();
     this.isAccessorType = this.stores.authentication.isAccessorType();
+    this.isAssessmentType = this.stores.authentication.isAssessmentType();
   }
+
   ngOnInit(): void {
     this.isInnovationInArchivedStatus = this.innovationCardData.status === InnovationStatusEnum.ARCHIVED;
 
@@ -72,9 +82,106 @@ export class InnovationAdvancedSearchCardComponent extends CoreComponent impleme
     this.diseasesAndConditionsList = this.getFormattedList(this.innovationCardData.diseasesAndConditions);
     this.keyHealthInequalitiesList = this.getFormattedList(this.innovationCardData.keyHealthInequalities);
     this.involvedAACProgrammesList = this.getFormattedList(this.innovationCardData.involvedAACProgrammes);
+
+    const searchTermsFoundWithDuplicates = this.getSearchTermsFound();
+    this.searchTermsFound = [...new Set(searchTermsFoundWithDuplicates)];
+    this.searchTermsCount = this.getSearchTermsCount(searchTermsFoundWithDuplicates);
+    this.snippet = this.getHighlightSnippet();
+    this.goToSectionLink = this.getGoToSectionLink();
   }
 
   private getFormattedList(list: string[]): string {
     return list.length > 0 ? list.join('. ') : 'None';
+  }
+
+  getSearchTermsFound() {
+    const searchTermsFound: string[] = [];
+
+    const highlights = this.innovationCardData.highlights;
+
+    if (!highlights) {
+      return searchTermsFound;
+    }
+
+    // Iterate over each value in the highlights object
+    for (const value of Object.values(highlights)) {
+      const text = value[0].toLowerCase();
+      // Use a regular expression to find all occurrences of text within <em> tags
+      const regex = /<em>(.*?)<\/em>/g;
+      let match;
+
+      // Loop through matches
+      while ((match = regex.exec(text)) !== null) {
+        // Add the matched text to our result array
+        searchTermsFound.push(match[1]);
+      }
+    }
+
+    return searchTermsFound;
+  }
+
+  getSearchTermsCount(searchTermsFoundWithDuplicates: string[]): Map<string, number> {
+    const searchTermsCount = new Map<string, number>();
+
+    // Count occurrences of each search term found
+    for (const word of searchTermsFoundWithDuplicates) {
+      const wordCapitalLetter = word.charAt(0).toUpperCase() + word.slice(1);
+      const currentCount = searchTermsCount.get(wordCapitalLetter) || 0;
+      searchTermsCount.set(wordCapitalLetter, currentCount + 1);
+    }
+
+    return searchTermsCount;
+  }
+
+  getHighlightSnippet(): string {
+    const highlights = this.innovationCardData.highlights;
+
+    if (!highlights) {
+      return '';
+    }
+
+    // Remove <em> tags from the first highlight given by ES
+    const snippet = Object.values(highlights)[0][0].replace(/<em>(.*?)<\/em>/g, '$1');
+
+    return snippet;
+  }
+
+  getGoToSectionLink(): { text: string; link: string; queryParams: { search: string } } {
+    let goToSectionLink = {
+      text: '',
+      link: '',
+      queryParams: { search: this.searchTermsFound.join(' ') }
+    };
+
+    const highlights = this.innovationCardData.highlights;
+
+    if (!highlights) {
+      return goToSectionLink;
+    }
+
+    // Get the first key from highlight given by ES
+    const firstKey = Object.keys(highlights)[0];
+    const keyParts = firstKey.split('.');
+
+    const innovationUrl = `/${this.baseUrl}${this.innovationCardData.id}`;
+
+    if (keyParts[0] === 'document') {
+      // If the key starts with document, it has data from IR
+      const sectionId = keyParts[1];
+      const sectionIdentification = this.stores.innovation.getInnovationRecordSectionIdentification(sectionId);
+      goToSectionLink.text = `Go to section ${sectionIdentification?.group.number}.${sectionIdentification?.section.number} ${sectionIdentification?.section.title}`;
+
+      if (this.isInnovationInArchivedStatus) {
+        goToSectionLink.link = `${innovationUrl}/record/sections/all`;
+      } else {
+        goToSectionLink.link = `${innovationUrl}/record/sections/${sectionId}`;
+      }
+    } else if (keyParts[0] === 'owner') {
+      // If the key starts with 'owner', it has data from overview
+      goToSectionLink.text = 'Go to innovation overview';
+      goToSectionLink.link = `${innovationUrl}/overview`;
+    }
+
+    return goToSectionLink;
   }
 }
