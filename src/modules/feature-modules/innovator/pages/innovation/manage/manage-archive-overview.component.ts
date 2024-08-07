b89e892889261b2e206bace8a1b695aee6e867fc
eduarda-meirinhos-bjss <eduarda.meirinhos@bjss.com>
2024-08-07 16:29:06 +0100

    Update go back behaviour for each flow

--- src/modules/feature-modules/innovator/pages/innovation/manage/manage-archive-overview.component.ts
+++ src/modules/feature-modules/innovator/pages/innovation/manage/manage-archive-overview.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { CoreComponent } from '@app/base';
 import { ContextInnovationType } from '@app/base/types';
+import { FormFieldActionsEnum } from '../how-to-proceed/how-to-proceed.component';
 
 @Component({
   selector: 'app-innovator-pages-innovation-manage-archive-overview',
@@ -10,21 +11,40 @@ import { ContextInnovationType } from '@app/base/types';
 })
 export class PageInnovationManageArchiveOverviewComponent extends CoreComponent implements OnInit {
   innovationId: string;
-
   innovation: ContextInnovationType;
+
+  action: FormFieldActionsEnum;
 
   constructor(private activatedRoute: ActivatedRoute) {
     super();
 
     this.innovationId = this.activatedRoute.snapshot.params.innovationId;
+    this.action = this.activatedRoute.snapshot.queryParams.action;
 
     this.innovation = this.stores.context.getInnovation();
 
-    this.setBackLink('Go back', `/innovator/innovations/${this.innovationId}/manage/innovation`);
+    this.setGoBackLink();
   }
 
   ngOnInit() {
     this.setPageTitle(`Archive ${this.innovation.name} innovation`);
     this.setPageStatus('READY');
+  }
+
+  setGoBackLink(): void {
+    const previousUrl = this.stores.context.getPreviousUrl();
+    if (previousUrl?.includes('how-to-proceed')) {
+      const howToProceedUrl = previousUrl.split('?')[0];
+      this.setBackLink('Go back', () => {
+        this.redirectTo(
+          howToProceedUrl,
+          this.action && {
+            action: this.action
+          }
+        );
+      });
+    } else {
+      this.setBackLink();
+    }
   }
 }
