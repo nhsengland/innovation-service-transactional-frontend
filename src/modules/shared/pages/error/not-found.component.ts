f6f021e367d0f7cea8579f3a990679a467c51eb0
eduarda-meirinhos-bjss <eduarda.meirinhos@bjss.com>
2024-10-30 16:09:40 +0000

    Set response status as 404 at not found component

--- src/modules/shared/pages/error/not-found.component.ts
+++ src/modules/shared/pages/error/not-found.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { Response } from 'express';
 
 import { CoreComponent } from '@app/base';
+import { RESPONSE } from 'src/express.tokens';
 
 @Component({
   selector: 'shared-pages-not-found',
   templateUrl: './not-found.component.html'
 })
-export class PageNotFoundComponent extends CoreComponent {
-  constructor() {
+export class PageNotFoundComponent extends CoreComponent implements OnInit {
+  constructor(@Optional() @Inject(RESPONSE) private response: Response) {
     super();
     this.setPageTitle('features.shared_pages.page_not_found.title');
+  }
+
+  ngOnInit(): void {
+    if (this.isRunningOnServer()) {
+      this.response?.status(404);
+    }
   }
 }
