e3c2b8084ccf537e74bf7402521377ad1b836b3c
pablocodeglia <4017664+pablocodeglia@users.noreply.github.com>
2024-11-07 15:42:47 +0000

    Creates 'recommend-needs-reassessment' component

---
+++ src/modules/feature-modules/innovator/pages/innovation/how-to-proceed/recommend-need-reassessment/recommend-needs-reassessment.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CoreComponent } from '@app/base';
+
+@Component({
+  selector: 'app-innovator-pages-recommend-needs-reassessment',
+  templateUrl: './recommend-needs-reassessment.html'
+})
+export class PageInnovationRecommendNeedsReassessment extends CoreComponent {
+  innovationId: string;
+  baseUrl: string;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    super();
+
+    this.innovationId = this.activatedRoute.snapshot.params.innovationId;
+
+    this.baseUrl = `/innovator/innovations/${this.innovationId}`;
+
+    this.setPageTitle('We recommend you submit for needs reassessment');
+    this.setBackLink('Go back', `${this.baseUrl}/how-to-proceed`);
+
+    this.setPageStatus('READY');
+  }
+
+  handleCancel() {
+    this.redirectTo(`${this.baseUrl}`);
+  }
+
+  onContinue() {
+    this.redirectTo('`${this.baseUrl}/how-to-proceed/needs-reassessment-send`');
+  }
+}
