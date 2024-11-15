0de8550307326025db8b6e404640cb716821ef27
pablocodeglia <4017664+pablocodeglia@users.noreply.github.com>
2024-11-15 11:09:42 +0000

    Updates surveys link

--- src/modules/shared/services/notifications.service.ts
+++ src/modules/shared/services/notifications.service.ts
@@ -327,7 +327,10 @@ export class NotificationsService extends CoreService {
                   };
                   break;
                 case NotificationContextDetailEnum.AU12_INNOVATOR_SURVEY_END_SUPPORT_TWO_MONTHS_REMINDER:
-                  link = { label: 'Click to go to satisfaction survey', url: `/${this.userUrlBasePath()}/` }; // Add satisfaction survey link
+                  link = {
+                    label: 'Click to go to satisfaction survey',
+                    url: `/${this.userUrlBasePath()}/innovations/${item.innovation.id}/surveys`
+                  };
                   break;
               }
               break;
