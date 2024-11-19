f4783875b38c27d947c89004b949c81d5622597b
pablocodeglia <4017664+pablocodeglia@users.noreply.github.com>
2024-11-19 10:30:51 +0000

    remove forgotten 'replace'

--- src/modules/feature-modules/innovator/pages/innovation/how-to-proceed/how-to-proceed.component.ts
+++ src/modules/feature-modules/innovator/pages/innovation/how-to-proceed/how-to-proceed.component.ts
@@ -60,11 +60,7 @@ export class PageInnovationHowToProceedComponent extends CoreComponent implement
       next: response => {
         this.formfieldAction = {
           title: 'Do you need more support to develop your innovation now?',
-          description:
-            `If you do not make a decision your innovation will be archived automatically on ${this.datePipe.transform(response.expectedArchiveDate, this.translate('app.date_formats.long_date'))}. You can continue to edit and update your innovation record when it is archived.`.replace(
-              '',
-              ''
-            ),
+          description: `If you do not make a decision your innovation will be archived automatically on ${this.datePipe.transform(response.expectedArchiveDate, this.translate('app.date_formats.long_date'))}. You can continue to edit and update your innovation record when it is archived.`,
           items: [
             {
               value: FormFieldActionsEnum.NEED_MORE_SUPPORT_NOW,
