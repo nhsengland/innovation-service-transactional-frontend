12b7ca31629e43960f01a06348b4e826a9c8250b
Diogo Costa <115171210+diogo-costa-bjss@users.noreply.github.com>
2024-12-03 11:56:52 +0000

    task: removed unecessary api call on user details update (#1581)
    - Instead of doing a call to the API to get the user info again, it nows
      update the state when the updateUser occurs.
    - Refactores the manage-details-info to be more reactive to state
      changes.
    - Fixes a bug where the updated information was not being displayed ont
      eh manage-details-info

--- src/modules/shared/pages/account/manage-details/manage-details-info.component.ts
+++ src/modules/shared/pages/account/manage-details/manage-details-info.component.ts
@@ -1,81 +1,81 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, computed } from '@angular/core';
 
 import { CoreComponent } from '@app/base';
 import { UtilsHelper } from '@app/base/helpers';
 import { UserRoleType } from '@modules/shared/dtos/roles.dto';
+import { UserContextType } from '@modules/stores';
+
+type SummaryList = { label: string; value: null | string; editStepNumber?: number }[];
 
 @Component({
   selector: 'shared-pages-account-manage-details-info',
   templateUrl: './manage-details-info.component.html'
 })
-export class PageAccountManageDetailsInfoComponent extends CoreComponent implements OnInit {
-  module: '' | 'innovator' | 'accessor' | 'assessment' = '';
+export class PageAccountManageDetailsInfoComponent extends CoreComponent {
+  summary = computed(() => {
+    const user = this.ctx.user.getUserInfo();
+    if (this.ctx.user.isInnovator()) return this.handleInnovatorInfo(user);
+    if (this.ctx.user.isAccessorOrAssessment()) return this.handleAccessorOrAssessmentInfo(user);
+    if (this.ctx.user.isAdmin()) return this.handleAdminInfo(user);
+    return [];
+  });
 
-  summaryList: { label: string; value: null | string; editStepNumber?: number }[] = [];
-
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor() {
     super();
     this.setPageTitle('Your details');
-
-    this.module = this.activatedRoute.snapshot.data.module;
+    this.setPageStatus('READY');
   }
 
-  ngOnInit(): void {
-    const user = this.ctx.user.getUserInfo();
+  private handleInnovatorInfo(user: UserContextType['user']): SummaryList {
+    let editStep = 0;
+    const summary = [
+      { label: 'Name', value: user.displayName, editStepNumber: ++editStep },
+      { label: 'Email address', value: user.email },
+      {
+        label: 'Contact preference',
+        value: UtilsHelper.getContactPreferenceValue(
+          user.contactByEmail,
+          user.contactByPhone,
+          user.contactByPhoneTimeframe
+        ),
+        editStepNumber: ++editStep
+      },
+      { label: 'Phone number', value: user.phone, editStepNumber: ++editStep },
+      { label: 'Contact details', value: user.contactDetails, editStepNumber: ++editStep }
+    ];
 
-    if (this.ctx.user.isInnovator()) {
-      this.summaryList = [
-        { label: 'Name', value: user.displayName, editStepNumber: 1 },
-        { label: 'Email address', value: user.email },
-        {
-          label: 'Contact preference',
-          value: UtilsHelper.getContactPreferenceValue(
-            user.contactByEmail,
-            user.contactByPhone,
-            user.contactByPhoneTimeframe
-          ),
-          editStepNumber: 2
-        },
-        { label: 'Phone number', value: user.phone, editStepNumber: 3 },
-        { label: 'Contact details', value: user.contactDetails, editStepNumber: 4 }
-      ];
-
-      if (!user.organisations[0].isShadow) {
-        this.summaryList.push({ label: 'Company', value: user.organisations[0].name, editStepNumber: 5 });
-        this.summaryList.push({
-          label: 'Company description',
-          value: user.organisations[0].description,
-          editStepNumber: 6
-        });
-        this.summaryList.push({ label: 'Company size', value: user.organisations[0].size, editStepNumber: 7 });
-        this.summaryList.push({
-          label: 'Company UK registration number',
-          value: user.organisations[0].registrationNumber ?? '',
-          editStepNumber: 8
-        });
-      }
-    } else if (this.ctx.user.isAccessorOrAssessment()) {
-      // this assumes that the user only has one organisation as it's currently the business case
-      const organisation = user.roles.find(
-        (r): r is UserRoleType & { organisation: { name: string } } => r.organisation !== undefined
-      )?.organisation?.name;
-      const roles = [...new Set(user.roles.map(r => this.ctx.user.getRoleDescription(r.role)))].join('\n');
-
-      this.summaryList = [
-        { label: 'Name', value: user.displayName, editStepNumber: 1 },
-        { label: 'Email address', value: user.email },
-        ...(organisation ? [{ label: 'Organisation', value: organisation }] : []),
-        { label: 'Service roles', value: roles }
-      ];
-    } else if (this.ctx.user.isAdmin()) {
-      this.summaryList = [
-        { label: 'Name', value: user.displayName, editStepNumber: 1 },
-        { label: 'Email address', value: user.email },
-        { label: 'User type', value: this.ctx.user.getRoleDescription(user.roles[0].role as 'ADMIN') }
-      ];
+    if (!user.organisations[0].isShadow) {
+      const [org] = user.organisations;
+      summary.push(
+        { label: 'Company', value: org.name, editStepNumber: ++editStep },
+        { label: 'Company description', value: org.description, editStepNumber: ++editStep },
+        { label: 'Company size', value: org.size, editStepNumber: ++editStep },
+        { label: 'Company UK registration number', value: org.registrationNumber ?? '', editStepNumber: ++editStep }
+      );
     }
+    return summary;
+  }
 
-    this.setPageStatus('READY');
+  private handleAccessorOrAssessmentInfo(user: UserContextType['user']): SummaryList {
+    // this assumes that the user only has one organisation as it's currently the business case
+    const organisation = user.roles.find(
+      (r): r is UserRoleType & { organisation: { name: string } } => r.organisation !== undefined
+    )?.organisation?.name;
+    const roles = [...new Set(user.roles.map(r => this.ctx.user.getRoleDescription(r.role)))].join('\n');
+
+    return [
+      { label: 'Name', value: user.displayName, editStepNumber: 1 },
+      { label: 'Email address', value: user.email },
+      ...(organisation ? [{ label: 'Organisation', value: organisation }] : []),
+      { label: 'Service roles', value: roles }
+    ];
+  }
+
+  private handleAdminInfo(user: UserContextType['user']): SummaryList {
+    return [
+      { label: 'Name', value: user.displayName, editStepNumber: 1 },
+      { label: 'Email address', value: user.email },
+      { label: 'User type', value: this.ctx.user.getRoleDescription(user.roles[0].role as 'ADMIN') }
+    ];
   }
 }
