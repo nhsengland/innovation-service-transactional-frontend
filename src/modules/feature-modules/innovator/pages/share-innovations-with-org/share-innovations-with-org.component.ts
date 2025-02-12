97f49ef19baea62ecab5f2e583fa79beb99429c4
Manuel Sousa <manuel.sousa@bjss.com>
2025-02-12 12:46:10 +0000

    ...

--- src/modules/feature-modules/innovator/pages/share-innovations-with-org/share-innovations-with-org.component.ts
+++ src/modules/feature-modules/innovator/pages/share-innovations-with-org/share-innovations-with-org.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CoreComponent } from '@app/base';
 import { OrganisationsService } from '@modules/shared/services/organisations.service';
@@ -12,11 +12,11 @@ import { InnovatorService } from '../../services/innovator.service';
   imports: [ThemeModule]
 })
 export class ShareInnovationsWithOrgComponent extends CoreComponent implements OnInit {
-  organisation: {
+  organisation: WritableSignal<{
     id: string;
     name: string;
     summary: string | null;
-  };
+  }>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -25,25 +25,28 @@ export class ShareInnovationsWithOrgComponent extends CoreComponent implements O
   ) {
     super();
     this.setPageTitle('New support organisation sharing preferences');
-    this.organisation = { id: this.activatedRoute.snapshot.params.organisationId, name: '', summary: null };
+    this.organisation = signal({ id: this.activatedRoute.snapshot.params.organisationId, name: '', summary: null });
   }
   ngOnInit(): void {
-    this.organisationService.getOrganisationInfo(this.organisation.id).subscribe(org => {
-      this.organisation.name = org.name;
-      this.organisation.summary = org.summary;
+    this.organisationService.getOrganisationInfo(this.organisation().id).subscribe(org => {
+      this.organisation.set({
+        id: org.id,
+        name: org.name,
+        summary: org.summary
+      });
 
       this.setPageStatus('READY');
     });
   }
 
   onSubmit() {
-    this.innovatorService.shareAllInnovationsWithOrg(this.organisation.id).subscribe({
+    this.innovatorService.shareAllInnovationsWithOrg(this.organisation().id).subscribe({
       next: () => {
-        this.setAlertSuccess('Innovations shared with ' + this.organisation.name);
+        this.setAlertSuccess('Innovations shared with ' + this.organisation().name);
         this.router.navigateByUrl('/innovator/dashboard');
       },
       error: () => {
-        this.setAlertError('An error occurred while sharing innovations with ' + this.organisation.name);
+        this.setAlertError('An error occurred while sharing innovations with ' + this.organisation().name);
       }
     });
   }
